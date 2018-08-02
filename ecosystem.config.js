bcf7a1c108c281be8aef29b57208561bca045ab9
gick <gicquel.pierreyves@gmail.com>
2018-08-03 00:12:46 +0200

    new dep

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,19 +3,19 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [
+  apps: [
 
     // First application
     {
-      name      : 'comicServer',
-      script    : 'server.js',
+      name: 'comicServer',
+      script: 'server.js',
       env: {
         NODE_ENV: 'dev',
-        PORT:80
+        PORT: 80
       },
-      env_production : {
+      env_production: {
         NODE_ENV: 'production',
-        PORT:443
+        PORT: 443
       }
     }
   ],
@@ -24,24 +24,28 @@ module.exports = {
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy : {
-    production : {
-      user : 'gick',
-      key : '/home/gick/.ssh/gickjessie_rsa',
-      host : '51.15.135.233',
-      ref  : 'origin/master',
-      repo : 'git@github.com:gick/easy_comic_server.git',
-      path : '/home/gick/easy-comic',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+  deploy: {
+    production: {
+      user: 'gick',
+      key: '/home/gick/.ssh/gickjessie_rsa',
+      host: '51.15.135.233',
+      ref: 'origin/master',
+      repo: 'git@github.com:gick/easy_comic_server.git',
+      path: '/home/gick/easy-comic',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      env: {
+        "NODE_ENV": "production"
+      }
+
     },
-    dev : {
-      user : 'gick',
-      key : '/home/gick/.ssh/gickjessie_rsa',
-      host : '51.15.135.233',
-      ref  : 'origin/master',
-      repo : 'git@github.com:gick/easy_comic_server.git',
-      path : '/home/gick/easy-comic-dev',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js',
+    dev: {
+      user: 'gick',
+      key: '/home/gick/.ssh/gickjessie_rsa',
+      host: '51.15.135.233',
+      ref: 'origin/master',
+      repo: 'git@github.com:gick/easy_comic_server.git',
+      path: '/home/gick/easy-comic-dev',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js',
     }
   }
 };
